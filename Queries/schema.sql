3b6def14fe930be5c837463f79f0a16371e18bf3
Muhammad Shahid <muhammad.s.shahid@outlook.com>
2022-06-22 21:27:45 -0400

    Create Retirement Info Table, filtered from Original Employee Data

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -35,13 +35,13 @@ CREATE TABLE salaries (
 	PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE managers (
-	dept_no VARCHAR(4) NOT NULL,
+CREATE TABLE dept_employee (
 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (dept_no, emp_no)
 );
 
@@ -50,9 +50,8 @@ CREATE TABLE titles (
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
--- Testing departments table
-SELECT * FROM departments;
+-- Testing table
+SELECT * FROM titles;
